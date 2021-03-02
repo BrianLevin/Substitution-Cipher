a0258337529b0593fe3216fb7743d5d1a4e5b1e6
BrianLevin <bml201095@gmail.com>
2021-03-02 13:27:57 -0500

    will hold the encrypted message

--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@
 using namespace std;
 
 int main() {
-
+// values to encrypt and decrypt message
  string alphabet {"[ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     string key  {" [XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
+
+
+// variable to store secret message
+  string secret_message {};
+    cout << "Enter your secret message : ";
+    getline(cin, secret_message); // getline= more then one word method
+
+    string encrypted_message {};
+
 
 return 0;
 
